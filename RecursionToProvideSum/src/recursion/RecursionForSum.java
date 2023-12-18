86e0e48a0972de50671e21f017294db4ebadbc0e
Phu Ha <phuha.engineer@gmail.com>
2023-12-17 20:10:43 -0500

    finish up the recursion class.

--- RecursionToProvideSum/src/recursion/RecursionForSum.java
+++ RecursionToProvideSum/src/recursion/RecursionForSum.java
@@ -1,9 +1,54 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class RecursionForSum {
 	
-	public static void main(String[] args) {
+	private final static int NUMBER_OF_NUM = 5;
+	
+	// recursion sum method
+	public static void recursionForSum(int numOfRecurse) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter you first number: ");
+		int sum = 0;
 		
+		// check to make sure user enter a valid integer
+		while (!scanner.hasNextInt()) {
+			System.out.println("Plase enter a valid integer: ");
+			scanner.next();
+		}
+		
+		sum += scanner.nextInt();
+		
+		//recursion call to helper method
+		recursionForSumHelper(numOfRecurse - 1, sum, scanner );
+	}
+	
+	// recursion sum helper method 
+	private static void recursionForSumHelper(int currDepth, int sum, Scanner scanner) {
+		// base case
+		if (currDepth == 0) {
+			System.out.println("The sum is " + sum + "!");
+		// recursive case
+		} else {
+			System.out.println("Please enter the next number: ");
+			
+			// check for valid integer input
+			while (!scanner.hasNextInt()) {
+				System.out.println("Plase enter a valid integer: ");
+				scanner.next();
+			}
+			
+			sum += scanner.nextInt();
+			
+			// recursion call
+			recursionForSumHelper(currDepth - 1, sum, scanner);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome! Please enter " + NUMBER_OF_NUM + " numbers for summing.");
+		recursionForSum(NUMBER_OF_NUM);
 	}
 
 }
